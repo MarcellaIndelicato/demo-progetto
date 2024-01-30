63355d9e71e4ec430249559cef67d08195b04b9b
MarcellaIndelicato <m.indelicato1@gmail.com>
2024-01-30 17:01:23 +0100

    Initial commit

---
+++ src/main/java/it/corso/DemoProgettoApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/corso/DemoProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
